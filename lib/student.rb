08cf1d786817ce22c9c6162791dbe409dda519ee
Alec German <alec_german@yahoo.com>
2018-03-16 01:46:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,7 +63,7 @@ class Student
       WHERE grade = '9'
       SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).map{|row| self.new_from_db(row)}
       self.new_from_db(row)
     end
   end
